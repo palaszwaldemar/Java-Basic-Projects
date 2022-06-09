198ad5bfcd6e572c5550b582c496920611d47530
palaszwaldemar <palasz.waldemar0@gmail.com>
2022-06-09 14:25:49 +0200

    Add print

--- src/zbijak/Launcher.java
+++ src/zbijak/Launcher.java
@@ -2,6 +2,8 @@ package zbijak;
 
 public class Launcher {
     public static void main(String[] args) {
+        Game game = new Game();
+        game.print();
         // - wyświetlenie planszy 10x10
         // - pojawienie się 4 graczy w różnych miejscach
         // - sterowanie W, A, S, D
