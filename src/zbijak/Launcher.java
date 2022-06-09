dcf750193a20eb4409af125e270ef5e999faa00c
palaszwaldemar <palasz.waldemar0@gmail.com>
2022-06-09 14:00:18 +0200

    Init reposytory

---
+++ src/zbijak/Launcher.java
@@ -0,0 +1,14 @@
+package zbijak;
+
+public class Launcher {
+    public static void main(String[] args) {
+        // - wyświetlenie planszy 10x10
+        // - pojawienie się 4 graczy w różnych miejscach
+        // - sterowanie W, A, S, D
+        // - nie można wypaść poza plansze
+        // - 3 graczy to npc, poruszają sie losowo
+        // - wejscie na pole innego gracza powoduje zbicie
+        // - komputer rusza się co dwa ruchy gracza
+        // - gra kończy się jak gracz zbije wszystkich lub sam zostanie zbity
+    }
+}
