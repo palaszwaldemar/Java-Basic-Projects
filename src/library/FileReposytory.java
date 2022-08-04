5b98e1fd2ea67b0b9c28d8995756223b1e43801a
palaszwaldemar <palasz.waldemar0@gmail.com>
2022-08-04 20:50:17 +0200

    correct spelling mistake

--- src/library/FileReposytory.java
+++ src/library/FileReposytory.java
@@ -37,7 +37,7 @@ public class FileReposytory {
         String[] text1 = text.split(",");
         int numberOfCopies = Integer.parseInt(text1[1]);
         if (numberOfCopies < 0 || numberOfCopies > 1000) {
-            throw new BookException("Niepoprwana liczba książek");
+            throw new BookException("Niepoprawna liczba książek");
         }
         if (text1[2].length() != 17) {
             throw new BookException("Niepoprawny numer ISBN");
@@ -52,6 +52,7 @@ public class FileReposytory {
         try {
             FileOutputStream fileOutputStream = new FileOutputStream("src\\library\\Ksiazki.csv", true);
             PrintWriter printWriter = new PrintWriter(fileOutputStream);
+            printWriter.print("\n");//todo czy może tak być. Dodałem przejście do nowej linii, ponieważ przy dodawaniu książki dodawało ją w ostatniej używanej linii
             printWriter.print(title + "," + numberOfCopies + "," + isbn);
             printWriter.close();
         } catch (FileNotFoundException e) {
