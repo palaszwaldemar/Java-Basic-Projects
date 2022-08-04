fa4a6031355c6e038fa6ca40f5be2bd808285880
palaszwaldemar <palasz.waldemar0@gmail.com>
2022-08-04 19:54:36 +0200

    add setters and getters, change constructor in Player class

--- src/zbijak/Player.java
+++ src/zbijak/Player.java
@@ -4,4 +4,37 @@ public class Player {
     private String name;
     private int x;
     private int y;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + name + '\'' +
+                ", x=" + x +
+                ", y=" + y +
+                '}';
+    }
 }
