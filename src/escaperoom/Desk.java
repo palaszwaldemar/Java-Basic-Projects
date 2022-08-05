c0cbab68dd31b9b7246ceb2c938afa7032849629
palaszwaldemar <palasz.waldemar0@gmail.com>
2022-08-05 16:32:31 +0200

    repair "use" method in case reuse object

--- src/escaperoom/Desk.java
+++ src/escaperoom/Desk.java
@@ -3,6 +3,8 @@ package escaperoom;
 public class Desk extends Item{
     private final Key keyToOpenDesk;
     private final Key keyToOpenDoor;
+    private boolean wasUse;
+    private boolean isOpen;
 
     public Desk(Key keyToOpenDesk, Key keyToOpenDoor) {
         super("Biurko");
@@ -12,8 +14,19 @@ public class Desk extends Item{
 
     @Override
     String use(Room room, Player player, Game game) {
+        if (wasUse) {
+            if(!isOpen) {
+                isOpen = true;
+                return "Otwierasz biurko";
+            } else {
+                isOpen = false;
+                return "Zamykasz biurko";
+            }
+        }
         if (player.hasItem(keyToOpenDesk)) {
             player.addItem(keyToOpenDoor);
+            isOpen = true;
+            wasUse = true;
             return "Znajdujesz klucz do drzwi";
         }
         return "Nie masz klucza do biurka";
