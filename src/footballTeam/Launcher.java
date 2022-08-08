503427125e924a05af3deaee1696d96810134ae1
palaszwaldemar <palasz.waldemar0@gmail.com>
2022-08-08 22:40:53 +0200

    add class "Launcher"

---
+++ src/footballTeam/Launcher.java
@@ -0,0 +1,8 @@
+package footballTeam;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Controller controller = new Controller();
+        controller.start();
+    }
+}
