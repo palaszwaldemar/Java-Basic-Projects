c171ed8da92350ffa445ff5a9c265d50788e16ad
palaszwaldemar <palasz.waldemar0@gmail.com>
2022-09-29 15:26:43 +0200

    added new project "zbijak"

--- src/zbijak/Launcher.java
+++ src/zbijak/Launcher.java
@@ -1,17 +1,18 @@
-package zbijak;//todo dokończyć
+package zbijak;
 
 public class Launcher {
     public static void main(String[] args) {
         Game game = new Game();
-        game.printBoard();
-        // - wyświetlenie planszy 10x10
-        // - pojawienie się 4 graczy w różnych miejscach
-        // - sterowanie W, A, S, D
+        game.showTable();
+        game.move();
+        game.showTable();
+        // - wyświetlenie planszy 10x10 +
+        // - pojawienie się 4 graczy w różnych miejscach +
+        // - sterowanie W, A, S, D +
         // - nie można wypaść poza plansze
-        // - 3 graczy to npc, poruszają sie losowo
+        // - 3 graczy to npc, poruszają sie losowo +
         // - wejscie na pole innego gracza powoduje zbicie
         // - komputer rusza się co dwa ruchy gracza
         // - gra kończy się jak gracz zbije wszystkich lub sam zostanie zbity
     }
 }
-
--- src/zbijak/NpcPlayer.java
+++
@@ -1,8 +0,0 @@
-package zbijak;
-
-public class NpcPlayer extends Player{
-
-    public NpcPlayer(String name) {
-        super(name);
-    }
-}
