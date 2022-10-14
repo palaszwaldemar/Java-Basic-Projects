d0313d7c6befaec9ec2a96ef8c45dcdba544491f
palaszwaldemar <palasz.waldemar0@gmail.com>
2022-10-14 20:19:59 +0200

    changed order of methods

--- src/zbijak/Controller.java
+++ src/zbijak/Controller.java
@@ -3,27 +3,24 @@ package zbijak;
 public abstract class Controller {
     private final Game game = new Game();
 
-    private StringBuilder showTable() {
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int y = 0; y < game.getSIZE(); y++) {
-            for (int x = 0; x < game.getSIZE(); x++) {
-                stringBuilder.append(game.getFieldText(x, y)).append("   ");
+    void start() {
+        welcome();
+        boolean humanMove = true;
+        do {
+            if (humanMove) {
+                humanTeamMove();
+                humanMove = false;
+            } else {
+                npcTeamMove();
+                humanMove = true;
             }
-            stringBuilder.append("\n");
-        }
-        return stringBuilder;
+        } while (!game.getHumanTeam().isEmpty() && !game.getNpcTeam().isEmpty());
+        printMessage(showTable().toString());
+        endGame();
     }
 
     private void welcome() {
         printMessage("\nGRA ZBIJAK\nSTEROWANIE: W, A, S, D\n");
-    }
-
-    private void endGame() {
-        if (game.getHumanTeam().isEmpty()) {
-            printMessage("Niestety przegrałeś");
-        } else {
-            printMessage("Gratulacje. Wygrałeś");
-        }
     }
 
     private void humanTeamMove() {
@@ -42,20 +39,23 @@ public abstract class Controller {
         game.moveNpcTeam();
     }
 
-    void start() {
-        welcome();
-        boolean humanMove = true;
-        do {
-            if (humanMove) {
-                humanTeamMove();
-                humanMove = false;
-            } else {
-                npcTeamMove();
-                humanMove = true;
+    private StringBuilder showTable() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int y = 0; y < game.getSIZE(); y++) {
+            for (int x = 0; x < game.getSIZE(); x++) {
+                stringBuilder.append(game.getFieldText(x, y)).append("   ");
             }
-        } while (!game.getHumanTeam().isEmpty() && !game.getNpcTeam().isEmpty());
-        printMessage(showTable().toString());
-        endGame();
+            stringBuilder.append("\n");
+        }
+        return stringBuilder;
+    }
+
+    private void endGame() {
+        if (game.getHumanTeam().isEmpty()) {
+            printMessage("Niestety przegrałeś");
+        } else {
+            printMessage("Gratulacje. Wygrałeś");
+        }
     }
 
     abstract void printMessage(String message);
